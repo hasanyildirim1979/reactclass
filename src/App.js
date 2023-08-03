41a2134d4baad68c6dd67936bd51757604961d93
hasanyildirim <hasan1979.2023@gmail.com>
2023-08-03 09:18:36 +0300

    02.08

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import JSX from "./1-JSX/JSX"
+import Props from "./2-Props/Props"
+import Events from "./3-Events/Events"
+import Conditionals from "./4-Conditionals/Conditionals";
+import Lists from "./5-Lists/Lists";
+
 
 function App() {
   return (
     <div>
-      <JSX/>
+      {/* <JSX/> */}
+      {/* <Props/> */}
+      {/* <Events/> */}
+      {/* <Conditionals/> */}
+      <Lists/>
     </div>
   );
 }
